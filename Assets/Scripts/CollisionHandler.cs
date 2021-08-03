31ee3c04d110d233be0e184a51a62d065edee5b4
Hole0Hunter <55822785+Hole0Hunter@users.noreply.github.com>
2021-08-03 23:10:04 +0530

    Particle Effects, Scoreboard script
    1. Tinkering player and enemy explosion VFX
    2. Basic scoreboard script, UI not started yet

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
-    {
-        
-    }
-
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem explosionVFX;
     void OnTriggerEnter(Collider other)
     {
-            
+        StartCrashSequence();           
+    }
+
+    void StartCrashSequence()
+    {
+        explosionVFX.Play(true);
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currSceneIndex);
     }
 
     // Start is called before the first frame update
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject enemyExplosionVFX;
+    [SerializeField] Transform parent;
+
+    ScoreBoard scoreBoard;
+    [SerializeField]int scorePerHit = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void KillEnemy()
+    {
+        GameObject vfx = Instantiate(enemyExplosionVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 }
